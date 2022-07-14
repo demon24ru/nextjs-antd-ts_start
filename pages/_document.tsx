97636f87272a630980a1578f9600a6d10ca84740
demon-dem <ittek@inbox.ru>
2022-07-14 12:55:01 +0700

    upd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,10 +12,6 @@ export default class extends Document {
             <Html lang="ru">
             <Head>
                 <meta charSet="utf-8"/>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
-                />
                 <meta name="theme-color" content="#ffffff"/>
                 <link rel="manifest" href="/manifest.json"/>
                 <style>{`
